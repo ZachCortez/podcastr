c84b7adfca66aa0e93105873b0618d175d2753ba
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2024-06-11 20:56:34 -0700

    Add files via upload

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig ={
+     providers: [
+      {
+        domain: "https://outgoing-cat-9.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+};
+
+export default authConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,8 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'lovely-flamingo-139.convex.cloud',
+                protocol: 'https',
+                hostname: 'img.clerk.com',      
             }
         ]
     }
