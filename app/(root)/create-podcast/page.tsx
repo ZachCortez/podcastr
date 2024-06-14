8cb54460f447a9234e0a7f804afa666971eb9ad9
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2024-06-14 13:45:10 -0700

    Update page.tsx

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -120,7 +120,7 @@ const CreatePodcast = () => {
                 <FormItem className="flex flex-col gap-2.5">
                   <FormLabel className="text-16 font-bold text-white-1">Title</FormLabel>
                     <FormControl>
-                      <Input className="input-class focus-visible:ring-offset-orange-1" placeholder="SaturnBay Podcast" {...field} />
+                      <Input className="input-class focus-visible:ring-offset-orange-1" placeholder="Create a Title for your podcast" {...field} />
                     </FormControl>
                   <FormMessage className="text-white-1" />
                 </FormItem>
